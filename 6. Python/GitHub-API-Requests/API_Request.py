448767de34e7803e46ff4f3605fcabb7df501a4f
SudheerBarakers <120264688+SudheerBarakers@users.noreply.github.com>
2023-03-30 11:55:34 +0530

    Create API_Request.py

---
+++ 6. Python/GitHub-API-Requests/API_Request.py
@@ -0,0 +1,11 @@
+import requests
+
+def api_check():
+    url = "https://reqres.in/api/users"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
+print(api_check())
